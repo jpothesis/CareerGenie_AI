e4146b6d20c27753454b3b4c282c9de6ad42caa0
Avisriva16 <avikasrivastava16@gmail.com>
2025-08-10 16:50:26 +0530

    deleted backend files

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMe } = require('../controllers/userController');
-const { protect }  = require('../middleware/authMiddleware');
-
-router.get('/profile', protect, getMe); // protected route
-
-module.exports = router;
