4470e5d33a45509763dc8c7c1c88a47b13a63719
Avika Srivastava <avikasrivastava16@gmail.com>
2025-09-03 23:05:25 +0530

    Delete backend directory from the branch

--- backend/routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
-const { getDashboardAnalytics } = require("../controllers/analyticsController");
-
-router.get("/", protect, getDashboardAnalytics);
-
-module.exports = router;
--- backend/routes/careerAdvisorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateCareerAdvice } = require("../controllers/careerAdvisor.controller");
-
-// Route to handle career advice requests
-router.post("/advice", generateCareerAdvice);
-
-module.exports = router;
