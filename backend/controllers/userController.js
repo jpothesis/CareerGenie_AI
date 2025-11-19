11285ed95a49db6d51c3d426b5bb1187f15499c8
Avisriva16 <avikasrivastava16@gmail.com>
2025-11-19 13:50:22 +0530

    minor fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const getMe = (req, res) => {
     return res.status(401).json({ message: 'User not authenticated' });
   }
   
-    res.status(200).json(req.user); // `req.user` is set in authMiddleware
+    res.status(200).json({user : req.user}); // `req.user` is set in authMiddleware
   };
   
   module.exports = { getMe };
