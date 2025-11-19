e05f0b6cb2f7ca8127a66548630c5a00ff1cbf0b
Avisriva16 <avikasrivastava16@gmail.com>
2025-11-19 13:38:39 +0530

    minor changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../controllers/authController');
+const { getMe } = require('../controllers/userController');
+const { protect }  = require('../middleware/authMiddleware');
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/profile', protect, getMe);
 
 module.exports = router;
